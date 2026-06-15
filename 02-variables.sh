b56ee6a0b753e24eabeca7849f56e96374d78d06
noorbhasha-hajarath-vali <hajarathvalinoor@gmail.com>
2026-06-15 17:45:14 +0530

    02-variables.sh file added and updated

---
+++ 02-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#variables
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1: Hi $PERSON2, hw are you"
+echo "$PERSON2: Hello $PERSON1, i am good, how are you"
+echo "$PERSON1: Good, Thanks.. what are your plans on weekend"
+echo "$PERSON2: Planning to go for trecking $PERSON1, would you like to join"
+echo "$PERSON1: yes, i love trecking, can you confirm the date and time"
+echo "$PERSON2: Sure, i will message all the details to you. see you soon"
+echo "$PERSON1: see you soon, Bye $PERSON2"
