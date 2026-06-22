bad417e49f89bb61d1ab5bbe95f84d88efc5574f
noorbhasha-hajarath-vali <hajarathvalinoor@gmail.com>
2026-06-22 11:55:33 +0530

    File added

--- 12-exit-codes.sh
+++ 12-exit-codes.sh
@@ -4,12 +4,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Use sudo to install package"
+    exit 1
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "ERROR:: Failed to Install"
+    exit 1
 else
     echo "SUCCESS:: mysql package installed successfully"
 fi
