92f4acdee9e9d71d551a8bb77448514f337071a2
noorbhasha-hajarath-vali <hajarathvalinoor@gmail.com>
2026-06-24 11:08:31 +0530

    12 file updated

--- 12-exit-codes.sh
+++ 12-exit-codes.sh
@@ -16,3 +16,20 @@ else
     echo "SUCCESS:: tree package installed successfully"
 fi
 
+dnf install whois -y
+
+if [ $? -ne 0 ]; then
+    echo "ERROR:: whois package installation failed"
+    exit 1
+else
+    echo "SUCCESS:: whois package installed successfully"
+fi
+
+dnf install telnet -y
+
+if [ $? -ne 0 ]; then
+    echo "ERROR:: telnet package installation failed"
+    exit 1
+else
+    echo "SUCCESS:: telnet package installed successfully"
+fi
