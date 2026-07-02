578c3af128aa75d881bdb3d019e49bde41961128
noorbhasha-hajarath-vali <hajarathvalinoor@gmail.com>
2026-07-02 06:57:00 +0530

    Loops updated

--- 16-for-loop.sh
+++ 16-for-loop.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
 USERID=$(id -u)
+LOG_DIR=/var/log/shell-script
+mkdir -p $LOG_DIR
+SCRIPT_NAME=$(basename "$0" .sh)
+LOG_FILE=$LOG_DIR/$SCRIPT_NAME.log
 
 if [ $USERID -ne 0 ]; then
     echo "User doesn't have root privilages, Run with sudo"
     exit 1
 fi
 
+echo "Process started executing at $(date)" | tee -a &>>$LOG_FILE
+
 VALIDATE () {
-    dnf install $1 -y
+    dnf install $1 -y &>>$LOG_FILE
 
     if [ $? -ne 0 ]; then
-        echo "ERROR:: $1 package installation Failed"
+        echo "ERROR:: $1 package installation Failed" | tee -a $LOG_FILE
     else
-        echo "SUCCESS:: $1 package installed Successfully"
+        echo "SUCCESS:: $1 package installed Successfully" | tee -a $LOG_FILE
     fi
 }
 
-
-VALIDATE tree
-VALIDATE jq-devel
-VALIDATE 7zip
-
+for package in $@
+do
+    dnf list installed $package &>>$LOG_FILE
+    if [ $? -ne 0 ]; then
+        VALIDATE $package
+    else
+        echo "SKIPPING:: $package package already installed" | tee -a $LOG_FILE
+    fi
+done
